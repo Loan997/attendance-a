761f2ddd8466e2f6a01d639a5525eb0ea3c4672b
ayaka <suima4743@gmail.com>
2019-02-25 17:27:59 +0000

    ページタブの変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   
   # ページごとの完全なタイトルを返します。
   def full_title(page_title = '')
-    base_title = "勤怠管理システムB"
+    base_title = "勤怠管理システムA"
     if page_title.empty?
       base_title
     else

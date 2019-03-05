d4a9a0f6ed13915ebac7af7daae324c48e2a4d4e
ayaka <suima4743@gmail.com>
2019-03-05 18:23:59 +0000

    今月をデフォルトで表示するよう変更

--- app/controllers/approval_histories_controller.rb
+++ app/controllers/approval_histories_controller.rb
@@ -1,9 +1,10 @@
 class ApprovalHistoriesController < ApplicationController
+  require 'date'
   
   before_action :logged_in_user
   
   def index
-    @approval_histories = ApprovalHistory.where(user_id: current_user.id).where(date: "2019-2-1".in_time_zone.all_month).order(:date, {created_at: :desc})
+    @approval_histories = ApprovalHistory.where(user_id: current_user.id).where(date: Time.current.in_time_zone.all_month).order(:date, {created_at: :desc})
   end
   
   def search
